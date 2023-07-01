f40818a8f82dd2bb63a6e0d9e79dd87495066a0d
bruno santos <devbrunosantos@gmail.com>
2023-07-01 20:00:13 -0300

    feat: logo with link to home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { AppProps } from "next/app";
-import { globalStyles } from "../styles/global";
-
+import Link from "next/link";
 import Image from "next/image";
+
 import logoImg from "../assets/logoWhite.png";
+import { globalStyles } from "../styles/global";
 import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} width={150} alt="" />
+        <Link href="/">
+          <Image src={logoImg} width={150} alt="" />
+        </Link>
       </Header>
       <Component {...pageProps} />
     </Container>
